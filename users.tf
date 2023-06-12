150ef8dfb99ca18005761a49bf078e0c77cc5405
Olivia Brown <brownoli22@gmail.com>
2023-06-12 16:38:43 -0400

    Adding Mikasa, Eren, and Armin

---
+++ users.tf
@@ -0,0 +1,18 @@
+resource "okta_user" "eren" {
+	first_name = "Eren"
+	last_name = "Jaeger"
+	email = "eren.jaeger@oliviabrown.xyz"
+}
+
+resource "okta_user" "mikasa" {
+	first_name = "Mikasa"
+	last_name = "Ackerman"
+	email = "mikasa.ackerman@oliviabrown.xyz"
+}
+
+resource "okta_user" "armin" {
+	first_name = "Armin"
+	last_name = "Arlert"
+	email = "armin.arlert@oliviabrown.xyz"
+
+}
