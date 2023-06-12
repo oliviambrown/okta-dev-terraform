534530869d754b8bdb65a0a3afe760759d51f1fc
Olivia Brown <brownoli22@gmail.com>
2023-06-12 19:16:43 -0400

    Adding mongoDB info

--- applications.tf
+++ applications.tf
@@ -4,3 +4,13 @@ resource "okta_app_saml" "mongodb_atlas" {
 	saml_version = "2.0"
 	status = "ACTIVE"
 }
+
+resource "okta_app_saml_app_settings" "mongodb_atlas_app_settings" {
+	app_id = okta_app_saml.mongodb_atlas.id
+	settings = jsonencode(
+	{
+		"acsUrl": "https://auth.mongodb.com/sso/saml2/0oajh4dwibKWjlyIX297"
+		"audience": "https://www.okta.com/saml2/service-provider/spkkjmwtynyziasqpvwp"
+	}
+	)
+}
