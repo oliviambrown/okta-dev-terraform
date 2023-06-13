faea4d30096df6b0e360572a05f00964400f37a5
Olivia Brown <brownoli22@gmail.com>
2023-06-12 20:00:55 -0400

    Fixing the application settings

--- applications.tf
+++ applications.tf
@@ -9,8 +9,8 @@ resource "okta_app_saml_app_settings" "mongodb_atlas_app_settings" {
 	app_id = okta_app_saml.mongodb_atlas.id
 	settings = jsonencode(
 	{
-		"acsUrl": "https://auth.mongodb.com/sso/saml2/0oajh4dwibKWjlyIX297"
-		"audience": "https://www.okta.com/saml2/service-provider/spkkjmwtynyziasqpvwp"
+		"acsURL" = "https://auth.mongodb.com/sso/saml2/0oajh4dwibKWjlyIX297"
+		"audienceURI" = "https://www.okta.com/saml2/service-provider/spkkjmwtynyziasqpvwp"
 	}
 	)
 }
