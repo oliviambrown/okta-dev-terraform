4942b3d698ced2e9f9052e97badf54f14e7ea703
Olivia Brown <brownoli22@gmail.com>
2023-06-13 16:23:03 -0400

    Adding the rest of the autheticators

--- authenticators.tf
+++ authenticators.tf
@@ -11,3 +11,33 @@ resource "okta_authenticator" "okta_verify" {
 	}
 	)
 }
+
+resource "okta_authenticator" "okta_email" {
+	key = "okta_email"
+	name = "Okta Email"
+}
+
+resource "okta_authenticator" "okta_password" {
+	key = "okta_password"
+	name = "Okta Password"
+}
+
+resource "okta_authenticator" "phone_number" {
+	key = "phone_number"
+	name = "Phone Number"
+}
+
+resource "okta_authenticator" "webauthn" {
+	key = "webauthn"
+	name = "Webauthn"
+} 
+
+resource "okta_authenticator" "google_otp" {
+	key = "google_otp"
+	name = "Google OTP"
+}
+
+resource "okta_authenticator" "security_question" {
+	key = "security_question"
+	name = "Security Question"
+}
