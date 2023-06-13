64f9245411de297ad1e930d615b3bb869a3d7786
Olivia Brown <brownoli22@gmail.com>
2023-06-13 15:55:28 -0400

    Adding Okta Verify for the 1st time

---
+++ authenticators.tf
@@ -0,0 +1,13 @@
+resource "okta_authenticator" "okta_verify" {
+	key = "okta_verify"
+	name = "Okta Verify"
+	settings = jsonencode(
+	{
+		userVerification = "PREFERRED"
+		channelBinding = {
+			style = "NUMBER_CHALLENGE"
+			required = "HIGH_RISK_ONLY"
+		}
+	}
+	)
+}
