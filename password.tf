e822613725b7db7f919be55b2c571085745bfb87
Olivia Brown <brownoli22@gmail.com>
2023-06-14 15:02:53 -0400

    Cleaning up policy

--- password.tf
+++ password.tf
@@ -5,7 +5,6 @@ resource "okta_policy_password" "employee_policy" {
     #Inactive to see it's final form
     status = "INACTIVE"
     
-    
     groups_included = ["${data.okta_group.jaegerists.id}"]
 
     #Password conditions
@@ -20,12 +19,15 @@ resource "okta_policy_password" "employee_policy" {
     password_dictionary_lookup = true
     password_max_age_days = 0
     password_min_age_minutes = 60
+    
+    #Lockout conditions
     password_show_lockout_failures = true
+    password_max_lockout_attempts = 10
+    password_auto_unlock_minutes = 60
 
     #Recovery
   
 }
-
 data "otka_group" "jaegerists" {
     name = "Jaegerists"
 }
