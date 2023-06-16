7941943c26d8c59e9877fe9cd933e48e8397c0db
Olivia Brown <brownoli22@gmail.com>
2023-06-16 09:29:25 -0400

    Update group-membership.tf

--- group-membership.tf
+++ group-membership.tf
@@ -2,6 +2,6 @@ resource "okta_group_memberships" "ducks_membership" {
     group_id = okta_group.ducks.id
     users = [ 
         okta_user.armin.id,
-        okta.user.mikasa.id
+        okta_user.mikasa.id
     ]
 }
