cdd220da366e1c84dcab49b54f00705c48861a87
Olivia Brown <brownoli22@gmail.com>
2023-06-16 11:33:57 -0400

    Case sensitivity is valid

--- group-memberships.tf
+++ group-memberships.tf
@@ -1,5 +1,5 @@
 resource "okta_group_memberships" "ducks_membership" {
-    group_id = okta_group.ducks.id
+    group_id = okta_group.Ducks.id
     users = [ 
         okta_user.armin.id,
         okta_user.mikasa.id
