78555e988521412940b9c144a2910eddcfc69335
Olivia Brown <brownoli22@gmail.com>
2023-06-26 17:04:54 -0400

    fixed declaration

--- mongodb-assignment.tf
+++ mongodb-assignment.tf
@@ -1,5 +1,9 @@
 resource "okta_app_group_assignment" "mongodb_app_assignment" {
-    app_id = okta_app_saml.mongodb_atlas
-    group_id = okta_group.mongodb_access
+    app_id = okta_app_saml.mongodb_atlas.id
+    group_id = okta_group.mongodb_access.id
 
 }
+
+/*data "okta_app" "mongodb_atlas" {
+  name
+}*/
