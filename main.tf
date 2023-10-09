4c4a7cdacbd4ea2c46f874df24fa4e01ce9300ee
Olivia Brown <brownoli22@gmail.com>
2023-10-09 14:44:19 -0400

    OIDC API? instead of super admin API

--- main.tf
+++ main.tf
@@ -1,19 +1,10 @@
 variable "org_name" {}
-#variable "TF_VAR_API_TOKEN" {}
 variable "base_url" {}
-
-#Terraform Cloud API
-#variable "api_token" {}
+variable "client_id" {}
+variable "scopes" {}
 
 # Enable and configure the Okta provider
 terraform {
-   /*cloud {
-      organization = "olivia-org"
-      workspaces {
-        name = "okta-dev-terraform"
-      }
-    }*/
-
   required_providers {
     okta = {
       source  = "okta/okta"
@@ -25,9 +16,8 @@ terraform {
 provider "okta" {
     org_name = var.org_name
     base_url = var.base_url
-   #api_token = var.TF_VAR_API_TOKEN
-    
-    #Terraform Cloud API 
-    #api_token = var.api_token
+    client_id = var.client_id
+    scopes = var.scopes
+    private_key = "${path.module}/rsa.pem"
 }
 
