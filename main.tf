0e51c8bd63441e8d74d25c70ea77eb7fb92f3d49
Olivia Brown <brownoli22@gmail.com>
2023-10-09 17:47:08 -0400

    Correcting the state manually

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ provider "okta" {
     org_name = var.org_name
     base_url = var.base_url
     client_id = var.client_id
-    scopes = ["okta.groups.manage", "okta.users.manage", "okta.policies.manage", "okta.apps.manage", "okta.groups.read","okta.policies.read", "okta.users.read", "okta.apps.read", "okta.authenticators.manage", "okta.authenticators.read"]
+    scopes = var.scopes
     private_key = "/mnt/workspace/rsa.pem"
 }
 
