d59fbe38dbaafb0605a306c7bd995d446f2856b9
Olivia Brown <brownoli22@gmail.com>
2025-03-19 15:34:09 -0400

    okta

--- okta/main.tf
+++ okta/main.tf
@@ -15,8 +15,8 @@ provider "okta" {
     private_key = "/mnt/workspace/rsa.pem"
 }
 
-resource "okta_group" "okta-group" {
+/*resource "okta_group" "okta-group" {
   name = "okta-group"
   description = "${var.terraform-warning}"
   
-}
+}*/
