8094e565d5fc098cb2edfafe653efc92f0d0d9d1
Olivia Brown <brownoli22@gmail.com>
2025-03-19 15:35:33 -0400

    actually deleted

--- okta/main.tf
+++ okta/main.tf
@@ -14,9 +14,3 @@ provider "okta" {
     scopes = var.scopes
     private_key = "/mnt/workspace/rsa.pem"
 }
-
-/*resource "okta_group" "okta-group" {
-  name = "okta-group"
-  description = "${var.terraform-warning}"
-  
-}*/
