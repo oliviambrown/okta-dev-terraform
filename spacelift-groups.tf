8e8259489f2152cff6dfd5d15c7b410e4a50e157
Olivia Brown <brownoli22@gmail.com>
2025-03-19 09:59:15 -0400

    fixing stuff.

--- spacelift-groups.tf
+++
@@ -1,9 +0,0 @@
-resource "okta_group" "spacelift_project_star_access" {
-    name = "App-Spacelift-Project-Star-Access"
-    description = "Spacelift Project Star Access"
-}
-
-resource "okta_group" "spacelift_project_okta_access" {
-    name = "App-Spacelift-Project-Okta-Access"
-    description = "Spacelift Project Okta Access"
-}
