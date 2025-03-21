aa20406c832cb6bdee7c36b17e4905ddfc096ff1
Olivia Brown <brownoli22@gmail.com>
2025-03-21 10:44:41 -0400

    data sourcing

--- groups/main.tf
+++ groups/main.tf
@@ -15,7 +15,24 @@ provider "okta" {
     private_key = "/mnt/workspace/rsa.pem"
 }
 
+data "okta_user" "olivia" {
+  search {
+    name = "profile.login"
+    value = "olivia@oliviabrown.xyz"
+  }
+  
+}
 
+resource "okta_app_bookmark" "okta_ice" {
+    label = "Okta Ice"
+    url = "https://oktaice.cloud/login/index.html"
+  
+}
+resource "okta_app_user" "okta-ice-olivia" {
+    app_id = okta_app_bookmark.okta_ice.id
+    user_id = data.okta_user.olivia.id
+  
+}
 
 
 
