af66e47cfbcc8235a59bdf11d984b37c3904bc51
Olivia Brown <brownoli22@gmail.com>
2025-03-21 12:41:21 -0400

    not losing it

--- groups/main.tf
+++ groups/main.tf
@@ -20,14 +20,29 @@ resource "okta_app_bookmark" "okta_ice" {
     url = "https://oktaice.cloud/login/index.html"
   
 }
-
-data "okta_app" "ice_data" {
-  id = okta_app_bookmark.okta_ice.id
-}
-data "okta_user" "olivia_data" {
+data "okta_user" "olivia" {
   search {
-    expression = "profile.firstName eq \"Olivia\""
+    name = "profile.login"
+    value = "olivia@oliviabrown.xyz"
   }
+}
+data "okta_user" "hisu" {
+  search {
+    name = "profile.firstName"
+    value = "Historia"
+  }
+
+  search {
+    name = "profile.lastName"
+    value = "Reiss"
+  }
+  
+}
+
+resource "okta_app_user" "okta-ice-olivia" {
+    app_id = okta_app_bookmark.okta_ice.id
+    user_id = data.okta_user.hisu.id
+  
 }
 
 
