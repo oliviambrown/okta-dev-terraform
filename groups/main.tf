bea1533ee218f689a04cbce65187b8f9f23ed506
Olivia Brown <brownoli22@gmail.com>
2025-03-21 12:52:30 -0400

    not losing it

--- groups/main.tf
+++ groups/main.tf
@@ -15,42 +15,13 @@ provider "okta" {
     private_key = "/mnt/workspace/rsa.pem"
 }
 
-resource "okta_app_bookmark" "okta_ice" {
-    label = "Okta Ice"
-    url = "https://oktaice.cloud/login/index.html"
-  
-}
+
 data "okta_user" "olivia" {
   search {
     name = "profile.login"
     value = "olivia@oliviabrown.xyz"
   }
 }
-data "okta_user" "hisu" {
-  search {
-    name = "profile.firstName"
-    value = "Historia"
-  }
 
-  search {
-    name = "profile.lastName"
-    value = "Reiss"
-  }
-  
-}
-
-resource "okta_app_user" "okta-ice-hisu" {
-    app_id = okta_app_bookmark.okta_ice.id
-    username = data.okta_user.hisu.login
-    user_id = data.okta_user.hisu.id
-  
-}
-
-resource "okta_app_user" "okta-ice-olivia" {
-    app_id = okta_app_bookmark.okta_ice.id
-    username = data.okta_user.olivia.login
-    user_id = data.okta_user.olivia.id
-  
-}
 
 
