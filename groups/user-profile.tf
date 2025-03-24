bf800a87dc0c4c44e615e377c1168301e0982fb2
Olivia Brown <brownoli22@gmail.com>
2025-03-24 10:56:28 -0400

    understand array enum

--- groups/user-profile.tf
+++ groups/user-profile.tf
@@ -2,9 +2,19 @@ resource "okta_user_schema_property" "okta_user_test" {
   index = "tfProp"
   title = "Terraform"
   type = "string"
-  enum = ["alpha", "beta", "charlie"]
+  enum = ["alpha", "beta", "charlie", "delta"]
   description = "Property managed by TF"
   master = "OKTA"
   permissions = "READ_WRITE"
 
+}
+
+resource "okta_user_schema_property" "okta_user_array_enum" {
+   index = "arrayEnumTest"
+   title = "arrayEnum"
+   type = "string"
+   description = "${var.terraform-warning}"
+   array_enum = ["a", "b", "c", "d"]
+   master = "OKTA" 
+  
 }
