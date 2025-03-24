7edb2c386d29f5f283d1135acb076cf9b3b2315b
Olivia Brown <brownoli22@gmail.com>
2025-03-24 11:48:15 -0400

    Add military

--- groups/user-profile.tf
+++ groups/user-profile.tf
@@ -16,3 +16,28 @@ resource "okta_user_schema_property" "okta_user_schema_custom" {
       title = "bee"
     }
 }
+
+resource "okta_user_schema_property" "okta_user_schema_military" {
+    index = "militaryBranch"
+    title = "Military Branch"
+    type = "string"
+    description = "${var.terraform-warning}"
+    master = "OKTA"
+    permissions = "READ_ONLY"
+
+    enum = ["scouts", "garrison", "militiary_police"]
+    one_of {
+      const = "scouts"
+      title = "Survey Corps"
+    }
+    one_of {
+      const = "garrison"
+      title = "Garrison"
+    }
+
+    one_of {
+      const = "military_police"
+      title = "Military Police"
+    }
+}
+
