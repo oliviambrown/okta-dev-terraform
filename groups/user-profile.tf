01d523f94ec47957264391ce60b0b968cd4b5a7e
Olivia Brown <brownoli22@gmail.com>
2025-03-24 11:26:03 -0400

    array_enum

--- groups/user-profile.tf
+++ groups/user-profile.tf
@@ -8,3 +8,14 @@ resource "okta_user_schema_property" "okta_user_test" {
   permissions = "READ_WRITE"
 
 }
+
+resource "okta_user_schema_property" "okta_user_schema_custom" {
+    index = "customProperty"
+    title = "custom"
+    type = "string"
+    description = "${var.terraform-warning}"
+    master = "OKTA"
+    permissions = "READ_WRITE"
+
+    array_enum = ["a", "b", "c", "d"]
+}
