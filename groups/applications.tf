8450f1a103f673c39059138f7832dddfbf727b15
Olivia Brown <brownoli22@gmail.com>
2025-03-24 15:31:50 -0400

    link

--- groups/applications.tf
+++ groups/applications.tf
@@ -21,4 +21,26 @@ resource "okta_app_user" "spacelift-olivia" {
   
 }
 
+resource "okta_app_saml" "mongodb" {
+    label                            = "MongoDB Atlas"
+    preconfigured_app                = "mongodbcloudmanager"
+    saml_version                     = "2.0"
+    status                           = "ACTIVE"
+    user_name_template               = "$${source.login}"
+    user_name_template_type          = "BUILT_IN"
+    attribute_statements {
+        filter_type  = "STARTS_WITH"
+        filter_value = "App-Atlas-"
+        name         = "memberOf"
+        namespace    = "urn:oasis:names:tc:SAML:2.0:attrname-format:unspecified"
+        type         = "GROUP"
+        values       = []
+  }
+}
+
+import {
+  to = okta_app_saml.mongodb
+  id = "0oach7ofrr0y1QkcM5d7"
+}
+
 
