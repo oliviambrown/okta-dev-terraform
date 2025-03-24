5fe13e33052bb8ec93a732027e5398a20a7f810d
Olivia Brown <brownoli22@gmail.com>
2025-03-24 15:35:19 -0400

    got the id

--- groups/applications.tf
+++ groups/applications.tf
@@ -22,25 +22,22 @@ resource "okta_app_user" "spacelift-olivia" {
 }
 
 resource "okta_app_saml" "mongodb" {
-    label                            = "MongoDB Atlas"
-    preconfigured_app                = "mongodbcloudmanager"
-    saml_version                     = "2.0"
-    status                           = "ACTIVE"
-    user_name_template               = "$${source.login}"
-    user_name_template_type          = "BUILT_IN"
+    id = "0oach7ofrr0y1QkcM5d7"
+    label = "MongoDB Atlas"
+    preconfigured_app = "mongodbcloudmanager"
+    saml_version = "2.0"
+    status = "ACTIVE"
+    user_name_template = "$${source.login}"
+    user_name_template_type = "BUILT_IN"
     attribute_statements {
-        filter_type  = "STARTS_WITH"
+        filter_type = "STARTS_WITH"
         filter_value = "App-Atlas-"
-        name         = "memberOf"
-        namespace    = "urn:oasis:names:tc:SAML:2.0:attrname-format:unspecified"
-        type         = "GROUP"
-        values       = []
+        name = "memberOf"
+        namespace = "urn:oasis:names:tc:SAML:2.0:attrname-format:unspecified"
+        type = "GROUP"
+        values = []
   }
 }
 
-import {
-  to = okta_app_saml.mongodb
-  id = "0oach7ofrr0y1QkcM5d7"
-}
 
 
