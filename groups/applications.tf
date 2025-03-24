1b10c474b0a5047eb853fb5273d35435f8c51fbd
Olivia Brown <brownoli22@gmail.com>
2025-03-24 13:51:56 -0400

    import block

--- groups/applications.tf
+++ groups/applications.tf
@@ -1,15 +1,9 @@
 resource "okta_app_oauth" "spacelift" {
     label = "Spacelift"
     type = "web"
-    grant_types = ["authorization_code"]
-    redirect_uris  = ["https://oliviamariebrown.app.spacelift.io/oidc/exchange"]
-    response_types = ["code"]
-    client_id = "0oah7ikdm5OzdnyQH5d7"
-    consent_method = "REQUIRED"
-    status = "ACTIVE"
-    issuer_mode = "DYNAMIC"
-    refresh_token_leeway = 0
-    refresh_token_rotation = "STATIC"
-    omit_secret = false
-  
+}
+
+import {
+  id = "0oah7ikdm5OzdnyQH5d7"
+  to = okta_app_oauth.spacelift
 }
