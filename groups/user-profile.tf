7bc87960bf3978d119b645bdbf6f6bc3b7045ddf
Olivia Brown <brownoli22@gmail.com>
2025-03-24 10:37:04 -0400

    saved user profile

--- groups/user-profile.tf
+++
@@ -1,9 +0,0 @@
-resource "okta_user_base_schema_property" "okta_user_test" {
-   index = "terraformProp"
-   title = "Terrafrom Property"
-   type = "string"
-   description = "Attribute from Terraform"
-   array_enum = ["alpha", "beta", "charlie"]
-   master = "OKTA" 
-   permissions = "READ_WRITE"
-}
