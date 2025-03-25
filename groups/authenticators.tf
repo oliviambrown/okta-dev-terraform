5cf0e9c7e2f68f91cc713996e73ab4481b1b0c6b
Olivia Brown <brownoli22@gmail.com>
2025-03-25 09:02:48 -0400

    email only

--- groups/authenticators.tf
+++ groups/authenticators.tf
@@ -1,8 +1,3 @@
-resource "okta_authenticator" "google" {
-  key = "google_otp"
-  name = "Google Authenticator"
-  status = "INACTIVE"
-}
 resource "okta_authenticator" "email" {
   name = "Email"
   key = "okta_email"
