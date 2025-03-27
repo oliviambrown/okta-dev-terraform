1044d5350be6518581e347c7c7603abfb7b383f4
Olivia Brown <brownoli22@gmail.com>
2025-03-27 14:37:59 -0400

    giving up on importing

--- groups/passwordless.tf
+++ groups/passwordless.tf
@@ -11,10 +11,3 @@ resource "okta_app_signon_policy" "passless_auth_policy" {
   
 }
 
-resource "okta_app_signon_policy_rule" "passless_auth_policy" {
-    name = "Passwordless Rule"
-    policy_id = "${okta_app_signon_policy.passless_auth_policy.id}"
-    groups_included = ["${okta_group.passless_group.id}"]
-    status = "INACTIVE"
-    factor_mode = "2FA"
-}
