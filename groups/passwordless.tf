fbd4109e9fc3ef966f2b455692287abc1e5c148c
Olivia Brown <brownoli22@gmail.com>
2025-03-27 14:18:42 -0400

    signon rule

--- groups/passwordless.tf
+++ groups/passwordless.tf
@@ -7,5 +7,15 @@ resource "okta_group" "passless_group" {
 resource "okta_app_signon_policy" "passless_auth_policy" {
     name = "Passwordless Auth Policy"
     description = "${var.terraform-warning}"
+
+  
+}
+
+resource "okta_app_signon_policy_rule" "passless_auth_policy" {
+    name = "Passwordless Rule"
+    policy_id = "${okta_app_signon_policy.passless_auth_policy.id}"
+    groups_included = ["${okta_group.passless_group.id}"]
+    status = "INACTIVE"
+    factor_mode = "2FA"
   
 }
