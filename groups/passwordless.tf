e2eb5a22485aa424bf2178f7ec769f08eca902f0
Olivia Brown <brownoli22@gmail.com>
2025-03-27 11:54:40 -0400

    sign on policy

--- groups/passwordless.tf
+++ groups/passwordless.tf
@@ -3,3 +3,9 @@ resource "okta_group" "passless_group" {
     description = "${var.terraform-warning} Setting up for passwordless"
   
 }
+
+resource "okta_policy_signon" "passless_auth" {
+    name = "Sign On Policy"
+    description = "${var.terraform-warning}"
+  
+}
