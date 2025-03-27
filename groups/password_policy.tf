4d14168ace83f3d69eb4bc5ec72a84d5375914a2
Olivia Brown <brownoli22@gmail.com>
2025-03-27 10:25:10 -0400

    Active Directory policy

--- groups/password_policy.tf
+++ groups/password_policy.tf
@@ -1,5 +1,5 @@
 resource "okta_policy_password_default" "default_pw" {
-    password_min_length = 8
+    password_min_length = 9
     password_exclude_username = true
     password_dictionary_lookup = true
     password_history_count = 5
@@ -10,7 +10,7 @@ resource "okta_policy_password" "ad_pw" {
     name = "Active Directory Policy"
     status = "ACTIVE"
     auth_provider = "ACTIVE_DIRECTORY"
-    password_min_length = 8
+    password_min_length = 9
     password_exclude_username = true
     password_dictionary_lookup = true
     password_history_count = 5
