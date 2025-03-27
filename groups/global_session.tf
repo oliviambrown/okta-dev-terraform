98dd886a3edc271e707dd8c01b8822ebbe23b221
Olivia Brown <brownoli22@gmail.com>
2025-03-27 15:05:53 -0400

    GSP

---
+++ groups/global_session.tf
@@ -0,0 +1,15 @@
+resource "okta_policy_signon" "passless_global_signon_policy" {
+    name = "Passwordless Global Session Policy"
+    priority = 1
+    groups_included = [okta_group.passless_group.id]
+    status = "ACTIVE"
+  
+}
+
+resource "okta_policy_rule_signon" "passless_global_signon_rule" {
+    name = "Global Session Policy Rule"
+    primary_factor = "PASSWORD_IDP_ANY_FACTOR"
+    access = "ALLOW"
+    status = "ACTIVE"
+  
+}
--- groups/imports.tf
+++
@@ -1,4 +0,0 @@
-import {
-  to = okta_app_signon_policy_rule.passless_auth_policy
-  id = "rsto148504PGcBEYu5d7/rulo16ydnucqrQvJp5d7"
-}
