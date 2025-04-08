dbde53c2019b0e1d31cd76f3c8cff357bc137325
Olivia Brown <brownoli22@gmail.com>
2025-04-08 15:30:06 -0400

    added members

--- groups/passwordless_auth_app_policy.tf
+++ groups/passwordless_auth_app_policy.tf
@@ -15,7 +15,39 @@ resource "okta_app_signon_policy_rule" "passless_auth_policy" {
     name = "Passwordless Rule"
     policy_id = "${okta_app_signon_policy.passless_auth_policy.id}"
     groups_included = ["${okta_group.passless_group.id}"]
-    status = "INACTIVE"
+    status = "ACTIVE"
     factor_mode = "2FA"
 }
 
+resource "okta_group_memberships" "passless_members" {
+    group_id = okta_group.passless_group.id
+    users = [
+        okta_user.connie.id,
+        okta_user.jean.id,
+        okta_user.sasha.id
+    ]
+  
+}
+
+data "okta_user" "jean" {
+    search {
+      name = "profile.firstName"
+      value = "Jean"
+    }
+  
+}
+
+data "okta_user" "connie" {
+    search {
+      name = "profile.firstName"
+      value = "Connie"
+    }
+  
+}
+
+data "okta_user" "sasha" {
+  search {
+    name = "profile.firstName"
+    value = "Sasha"
+  }
+}
