8fcdc86788e7a790c3288b15bc212c63819b96e6
Olivia Brown <brownoli22@gmail.com>
2025-04-09 11:00:47 -0400

    viz stud

--- groups/admin_auth.tf
+++ groups/admin_auth.tf
@@ -4,9 +4,3 @@ resource "okta_group" "admin" {
 
 }
 
-resource "okta_group_memberships" "olivia-super-membership" {
-  group_id = okta_group.admin.id
-  users = [
-      data.okta_user.olivia.id
-  ]
-}
