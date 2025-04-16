a2a1261125792f0d11073f07f349d17c9f9b0d1d
Olivia Brown <brownoli22@gmail.com>
2025-04-16 15:38:05 -0400

    create the rule with nothing

--- okta/policy_signon_test.tf
+++ okta/policy_signon_test.tf
@@ -6,20 +6,5 @@ resource "okta_app_signon_policy" "test" {
 resource "okta_app_signon_policy_rule" "test" {
     policy_id = okta_app_signon_policy.test.id
     name      = "testAcc_replace_with_uuid"
-    constraints = [
-        jsonencode(
-            {
-                knowledge = {
-                    reauthenticateIn = "PT2H"
-                    types = [
-                        "password",
-                    ]
-                }
-                possession = {
-                    deviceBound        = "REQUIRED"
-                    hardwareProtection = "REQUIRED"
-                }
-            }
-        )
-    ]
+
 }
