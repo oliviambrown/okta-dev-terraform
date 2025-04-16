2fa029359978754e0c98fb4db8d27edabbcdeda3
Olivia Brown <brownoli22@gmail.com>
2025-04-16 09:14:39 -0400

    Adding new attribute

--- okta/spacelift.tf
+++ okta/spacelift.tf
@@ -1,5 +1,7 @@
 resource "okta_group" "space-group" {
-  name = "space_group"
-  description = "${var.terraform-warning}"
-  
+    name = "space_group"
+    description = "${var.terraform-warning}"
+    custom_profile_attributes = jsonencode({
+        "tfManaged" = true
+    })
 }
