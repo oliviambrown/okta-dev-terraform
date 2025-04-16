b39320a3e2b33a9ac4fa8ac475fef7c61417875c
Olivia Brown <brownoli22@gmail.com>
2025-04-16 13:53:15 -0400

    Inactive until finished

--- okta/policy_admin_console.tf
+++ okta/policy_admin_console.tf
@@ -12,5 +12,10 @@ data "okta_app_signon_policy" "console_auth_policy" {
 ### I want to add a new rule, not create a new policy
 resource "okta_app_signon_policy_rule" "console_policy_rule" {
     name = "rule_admin_super"
+    status = "INACTIVE"
     policy_id = data.okta_app_signon_policy.console_auth_policy.id
+    priority = 1
+    access = "ALLOW"
+    factor_mode = "2FA"
+    groups_included = [okta_group.admin.id]
 }
