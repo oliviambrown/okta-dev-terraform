845c0e3e21674edfb07231e3ed15be41cd7a0d08
Olivia Brown <brownoli22@gmail.com>
2025-04-16 15:11:45 -0400

    Idk what's wrong.

--- okta/policy_admin_console.tf
+++ okta/policy_admin_console.tf
@@ -19,10 +19,13 @@ resource "okta_app_signon_policy_rule" "console_policy_rule" {
     factor_mode = "2FA"
     groups_included = [okta_group.admin.id]
     constraints = [
-        jsonencode({
-            possession =  {
-                deviceBound = "REQUIRED"
+        jsonencode(
+            {
+                possession = {
+                    deviceBound        = "REQUIRED"
+                    hardwareProtection = "REQUIRED"
+                }
             }
-        })
+        )
     ]
 }
