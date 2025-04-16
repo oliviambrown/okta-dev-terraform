73d973fbe6979c4be0b7ccc6f1cdec6d8df437ac
Olivia Brown <brownoli22@gmail.com>
2025-04-16 14:55:16 -0400

    hardware protection on

--- okta/policy_admin_console.tf
+++ okta/policy_admin_console.tf
@@ -21,9 +21,7 @@ resource "okta_app_signon_policy_rule" "console_policy_rule" {
     constraints = [
         jsonencode({
             "posession" : {
-                "deviceBound" : "REQUIRED",
-                "hardwareProtection" : "REQUIRED",
-                "phishingResistant" : "REQUIRED"
+                "hardwareProtection" : "REQUIRED"
             }
         })
     ]
