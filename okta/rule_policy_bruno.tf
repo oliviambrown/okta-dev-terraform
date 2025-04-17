6383c4b6bf3a3030c8e45fcfa14c8febab963262
Olivia Brown <brownoli22@gmail.com>
2025-04-17 11:57:29 -0400

    bruno tf rule - rewrite

--- okta/rule_policy_bruno.tf
+++ okta/rule_policy_bruno.tf
@@ -1,16 +1,25 @@
 resource "okta_app_signon_policy_rule" "bruno-tf" {
     name      = "tf_rule"
     policy_id = data.okta_policy.bruno_policy.id
+
+    ### App Sign On API conversation
+
+    # appSignOn.access
     access = "ALLOW"
+
+    # appSignOn.factorMode
     factor_mode = "2FA"
+
+    # appSignOn.verificationMethod.type
     type = "ASSURANCE"
-    inactivity_period = "PT2H"
+
+    # appSignOn.verificationMethod.type.constraints
     constraints = [
         jsonencode({
-            possession = {
-                deviceBound        = "REQUIRED"
-                hardwareProtection = "REQUIRED"
-                phishingResistant  = "REQUIRED"
+            "possession" : {
+                "deviceBound" : "REQUIRED",
+                "hardwareProtection" : "REQUIRED"
+                "phishingResistant": "REQUIRED"
             }
         })
     ]
