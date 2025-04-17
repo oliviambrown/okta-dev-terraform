65ab7dd9851bb629502b373fac8f0821c6883c97
Olivia Brown <brownoli22@gmail.com>
2025-04-17 09:02:53 -0400

    device bound

--- okta/policy_signon_test.tf
+++ okta/policy_signon_test.tf
@@ -8,7 +8,9 @@ resource "okta_app_signon_policy_rule" "test" {
     name      = "testAcc_replace_with_uuid"
     factor_mode = "2FA"
     constraints = [
-        jsonencode()
+        jsonencode({
+            deviceBound = "REQUIRED"
+        })
     ]
 
 }
