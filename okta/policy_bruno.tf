37bf5ae84d9de13228400f1ac7dc713109e9d7d2
Olivia Brown <brownoli22@gmail.com>
2025-04-17 14:45:15 -0400

    matching states

--- okta/policy_bruno.tf
+++ okta/policy_bruno.tf
@@ -1,9 +1,3 @@
-resource "okta_app_signon_policy_rule" "bruno-tf" {
-    name      = "tf_rule"
-    policy_id = data.okta_policy.bruno_policy.id
-    factor_mode = "2FA"
-}
-
 data "okta_policy" "bruno_policy" {
     name   = "bruno"
     type   = "ACCESS_POLICY"
