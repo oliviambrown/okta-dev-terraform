fa4ed0b88a8238096b0b4c48dc258bca876381d2
Olivia Brown <brownoli22@gmail.com>
2025-04-17 14:18:34 -0400

    I really don't know.

--- okta/policy_bruno.tf
+++ okta/policy_bruno.tf
@@ -2,20 +2,11 @@ resource "okta_app_signon_policy_rule" "bruno-tf" {
     name      = "tf_rule"
     policy_id = data.okta_policy.bruno_policy.id
 
-    ### App Sign On API conversation
-
-    # appSignOn.access
-    access = "ALLOW"
-
-    # appSignOn.factorMode
     factor_mode = "2FA"
-
-    # appSignOn.verificationMethod.type
+    re_authentication_frequency = "PT12H"
     type = "ASSURANCE"
-
-    # appSignOn.verificationMethod.type.constraints
     constraints = [
-        jsonencode({ "possession" : { "required" : true, "hardwareProtection" : "REQUIRED" } })
+        jsonencode({"possession":{"required":true,"deviceBound":"REQUIRED"}}),
     ]
 }
 
